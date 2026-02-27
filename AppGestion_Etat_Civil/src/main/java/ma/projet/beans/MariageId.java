56fcb5b7a3007913819cbe0beb7808d0cfcb0a58
Hafsa-Belahnech <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-02-27 21:32:52 +0000

    Add files via upload

---
+++ AppGestion_Etat_Civil/src/main/java/ma/projet/beans/MariageId.java
@@ -0,0 +1,57 @@
+package ma.projet.beans;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class MariageId implements Serializable {
+
+    @Column(name = "idHomme")
+    private int idHomme;
+
+    @Column(name = "idFemme")
+    private int idFemme;
+
+    // Constructeur sans paramètres requis par JPA
+    public MariageId() {
+    }
+
+    // Constructeur avec paramètres - CORRIGÉ ⚠️
+    public MariageId(int idHomme, int idFemme) {
+        this.idHomme = idHomme;
+        this.idFemme = idFemme;
+    }
+
+    // GETTERS et SETTERS
+    public int getIdHomme() {
+        return idHomme;
+    }
+
+    public void setIdHomme(int idHomme) {
+        this.idHomme = idHomme;
+    }
+
+    public int getIdFemme() {
+        return idFemme;
+    }
+
+    public void setIdFemme(int idFemme) {
+        this.idFemme = idFemme;
+    }
+
+    // hashCode() & equals() - Obligatoires pour une clé composite
+    @Override
+    public int hashCode() {
+        return Objects.hash(idHomme, idFemme);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MariageId that = (MariageId) obj;
+        return idHomme == that.idHomme && idFemme == that.idFemme;
+    }
+}
