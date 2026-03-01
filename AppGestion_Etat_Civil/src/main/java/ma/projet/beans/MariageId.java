d45fe6f153ee7bd4584212a26a41c12d362d5015
Hafsa-Belahnech <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-01 20:53:41 +0000

    Update MariageId.java

--- AppGestion_Etat_Civil/src/main/java/ma/projet/beans/MariageId.java
+++ AppGestion_Etat_Civil/src/main/java/ma/projet/beans/MariageId.java
@@ -1,50 +1,49 @@
 package ma.projet.beans;
 
 import java.io.Serializable;
-import java.util.Objects;
-import javax.persistence.Column;
+import javax.persistence.*;
 import javax.persistence.Embeddable;
 
 @Embeddable //gerer le clé composite
 public class MariageId implements Serializable {
 
     @Column(name = "idHomme")
-    private int idHomme;
+    private int Homme;
 
     @Column(name = "idFemme")
-    private int idFemme;
+    private int Femme;
 
     // Constructeur sans paramètres requis par JPA
     public MariageId() {
     }
 
     // Constructeur avec paramètres 
-    public MariageId(int idHomme, int idFemme) {
-        this.idHomme = idHomme;
-        this.idFemme = idFemme;
+    public MariageId(int Homme, int Femme) {
+        this.Homme = Homme;
+        this.Femme = Femme;
     }
 
     // GETTERS et SETTERS
-    public int getIdHomme() {
-        return idHomme;
+    public int getHomme() {
+        return Homme;
     }
 
-    public void setIdHomme(int idHomme) {
-        this.idHomme = idHomme;
+    public void setHomme(int Homme) {
+        this.Homme = Homme;
     }
 
-    public int getIdFemme() {
-        return idFemme;
+    public int getFemme() {
+        return Femme;
     }
 
-    public void setIdFemme(int idFemme) {
-        this.idFemme = idFemme;
+    public void setFemme(int Femme) {
+        this.Femme = Femme;
     }
 
     // hashCode() & equals()
     @Override
     public int hashCode() {
-        return Objects.hash(idHomme, idFemme);
+        return Objects.hash(Homme, Femme);
     }
 
     @Override
@@ -52,7 +51,8 @@ public class MariageId implements Serializable {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         MariageId that = (MariageId) obj;
-        return idHomme == that.idHomme && idFemme == that.idFemme;
+        return Homme == that.Homme && Femme == that.Femme;
     }
 
 }
+
